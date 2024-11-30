43f31b3ad009bbe84a02388e7288902e7f7bbc9f
Nivedh K <nivedh.koya@gmail.com>
2024-11-30 02:23:09 -0600

    adding join group page with updates

--- classcohort/app/src/App.jsx
+++ classcohort/app/src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Signin from "./Signin.jsx";
 import Signup from "./Signup.jsx";
 import Homepage from "./homepage.jsx";
-import Create from "./create.jsx"; 
+import Create from "./create.jsx";
 import Chat from "./chat.jsx";
 import Select from "./Select.jsx"
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import JoinGroup from "./join.jsx";
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/app" element={<Homepage />} />
-        <Route path="/create" element={<Create />} /> 
+        <Route path="/create" element={<Create />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/join" element={<JoinGroup />} />
         <Route path="/select" element={<Select />} />
       </Routes>
     </Router>
--- classcohort/app/src/homepage.jsx
+++ classcohort/app/src/homepage.jsx
@@ -28,7 +28,7 @@ const Homepage = () => {
 
           {/* Buttons */}
           <div className="flex justify-center space-x-4 mt-4">
-          <button
+            <button
               onClick={() => navigate("/create")}
               className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600"
             >
@@ -39,6 +39,12 @@ const Homepage = () => {
               className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
             >
               Select
+            </button>
+            <button
+              onClick={() => navigate("/join")}
+              className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
+            >
+              Join
             </button>
           </div>
         </div>
